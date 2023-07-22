ca983633ad5429cbc6cc2de4b2b7ccfaded97ca6
musa <musa>
2023-07-22 08:01:45 +0200

    updated rds.tf

--- rds.tf
+++ rds.tf
@@ -1,12 +1,11 @@
 resource "aws_db_instance" "default" {
   allocated_storage    = 5
-  storage_type         = "gp2"
+  db_name              = "mydb"
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
-  db_name              = "mydb"
   username             = "foo"
-  password             = "${file("../rds_pass.txt")}"
+  password             = "${trimspace(file("../rds_pass.txt"))}"
   parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot = "true"
+  skip_final_snapshot  = true
 }
